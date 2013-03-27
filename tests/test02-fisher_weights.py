69b08293240a445ba81320e070d47411c5ccc457
igg@nih.gov <igg@nih.gov@d860a611-dd6a-fbde-3e4e-7fa7191099b5>
2013-03-27 03:34:53 +0000

    Added normalization sub-test in fischer weights. Tightened tolerances
    git-svn-id: http://wnd-charm.googlecode.com/svn/pychrm/trunk@696 d860a611-dd6a-fbde-3e4e-7fa7191099b5

--- tests/test02-fisher_weights.py
+++ tests/test02-fisher_weights.py
@@ -25,20 +25,52 @@ if not (test_dir):
 from pychrm import __version__ as pychrm_version
 # -------- END preamble to get the test data --------------------
 
-test_name = "Fisher weights"
-max_diff_pass = 0.002
-max_mean_pass = 0.000001
 test_fit = os.path.join (test_dir,'test-l.fit')
 test_fit_wght = os.path.join (test_dir,'test_fit-l.weights')
 
 ts = FeatureSet_Discrete.NewFromFitFile( test_fit )
 ts.Normalize()
-calc_wghts = FisherFeatureWeights.NewFromFeatureSet (ts)
+	
 
+calc_wghts = FisherFeatureWeights.NewFromFeatureSet (ts)
+# test weights generated from test-l.fit:
+# wndchrm classify -l -f1.0 -vtest_fit-l.weights test-l.fit test-l.fit 
 
 test_wghts = FisherFeatureWeights.NewFromFile (test_fit_wght)
 
-epsilon = 0.00001
+test_name = "Normalization"
+max_diff_pass = 0.0001
+max_mean_pass = 0.00005
+epsilon = 0.00005
+max_diff = 0.
+sum_diff = 0.
+num_diffs = 0.
+
+ts_norm = FeatureSet_Discrete.NewFromFitFile( os.path.join (test_dir,'test_fit-l-normalized.fit') )
+for img_idx in range (ts.num_images):
+	for f_idx in range (ts.num_features):
+		test_val = ts_norm.data_matrix[img_idx][f_idx]
+		calc_val = ts.data_matrix[img_idx][f_idx]
+		diff = abs(calc_val - test_val)
+		sum_diff += diff
+		num_diffs += 1.0
+		if diff > max_diff:
+			max_diff = diff
+		if ( diff > epsilon):
+			print "computed normalization for '{0}' ({1}) differs from normalization in file ({2}) by {3}".format (
+				ts.featurenames_list[f_idx], calc_val, test_val, diff )
+
+print "{0} comparissons, maximum diff = {1}, mean = {2}".format ( int (num_diffs), max_diff, sum_diff / num_diffs )
+if (max_diff > max_diff_pass or (sum_diff / num_diffs) > max_mean_pass):
+	print "pychrm {0} {1} test: {2}".format (pychrm_version, test_name, 'FAIL')
+else:
+	print "pychrm {0} {1} test: {2}".format (pychrm_version, test_name, 'PASS')
+
+
+test_name = "Fisher weights"
+max_diff_pass = 0.0001
+max_mean_pass = 0.00005
+epsilon = 0.00004
 max_diff = 0.
 sum_diff = 0.
 num_diffs = 0.
