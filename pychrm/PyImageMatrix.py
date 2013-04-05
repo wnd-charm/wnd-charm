1366323a46b32d3791b4a475d3837b5073939fed
igg@nih.gov <igg@nih.gov@d860a611-dd6a-fbde-3e4e-7fa7191099b5>
2013-04-05 14:21:01 +0000

    blocked out not-quite-working strides for now. Made imports work within current directory location
    git-svn-id: http://wnd-charm.googlecode.com/svn/pychrm/trunk@710 d860a611-dd6a-fbde-3e4e-7fa7191099b5

--- pychrm/PyImageMatrix.py
+++ pychrm/PyImageMatrix.py
@@ -1,8 +1,9 @@
-from pychrm.FeatureSet import *
+import pychrm
+from pychrm import ImageMatrix
 import numpy as np
 import ctypes
 
-class PyImageMatrix (pychrm.ImageMatrix):
+class PyImageMatrix (ImageMatrix):
 	bytes_per_double = np.dtype(np.double).itemsize
 	def __init__(self):
 		super(PyImageMatrix, self).__init__()
@@ -15,14 +16,23 @@ class PyImageMatrix (pychrm.ImageMatrix):
 		#
 		# works, full control over ndarray constructor
 		n_mem_doubles = self.width * self.height
-		stride_bytes = (self.width * PyImageMatrix.bytes_per_double, self.height * PyImageMatrix.bytes_per_double)
 		ap = ctypes.cast(int(self.data_ptr()), ctypes.POINTER(ctypes.c_double * n_mem_doubles))
-		return np.ndarray(shape=(self.height,self.width), dtype = np.double, strides=stride_bytes, buffer=ap.contents)
+# strides aren't quite right yet...
+# 		stride_bytes = (self.width * PyImageMatrix.bytes_per_double, self.height * PyImageMatrix.bytes_per_double)
+# 		return np.ndarray(shape=(self.height,self.width), dtype = np.double, strides=stride_bytes, buffer=ap.contents)
+		return np.ndarray(shape=(self.height,self.width), dtype = np.double, buffer=ap.contents)
 if __name__ == "__main__":
-	foo = PyImageMatrix()
-	foo.allocate (100,100)
-	bar = foo.as_ndarray()
-	print "The numpy returned by as_ndarray()"
-	print dir (bar)
-	foo = None
+	im = PyImageMatrix()
+	im.allocate (200,200)
+	mat = im.as_ndarray()
+	print "mat = im.as_ndarray()"
+	print mat
+	print "mat.ctypes.data: {0:x}".format(mat.ctypes.data)
+	mat[:] = 0.
+	print "mat.ctypes.data: {0:x} after mat[:] = 0".format(mat.ctypes.data)
+	print mat
+	print "deallocating mat"
+	mat = None
+	print "deallocating im"
+	im = None
 	print "last line"
