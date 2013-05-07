877ae54b8762bd03d167eeefd5354c2a867d60c1
christopher.coletta@nih.gov <christopher.coletta@nih.gov@d860a611-dd6a-fbde-3e4e-7fa7191099b5>
2013-05-07 19:02:21 +0000

    Adding more options for redirecting output
    git-svn-id: http://wnd-charm.googlecode.com/svn/pychrm/trunk@722 d860a611-dd6a-fbde-3e4e-7fa7191099b5

--- examples/generate_graphs_from_c-chrm_HTML_report.py
+++ examples/generate_graphs_from_c-chrm_HTML_report.py
@@ -39,36 +39,47 @@ import os
 parser = argparse.ArgumentParser( description="Takes a C-chrm-generated HTML report and generates a kernel-smoothed probability density estimate graph or a rank-ordered predicted values graph")
 parser.add_argument( 'html_report_filepath', help='path WND-CHARM HTML report',
                      nargs=1 )
-parser.add_argument( 'graph_output_filepath', help='Base string used for naming outputted .png graphs',
+parser.add_argument( '--figure_basename', metavar='<string>', help='Base string used for naming outputted .png graphs',
                      nargs='?', default = None )
-parser.add_argument( 'debug', help='Name of file into which HTML parsing output and aggregation output will go.',
-                     nargs='?', default = None )
+parser.add_argument( '--debug', metavar='<optional path>', help='Specify where to send parsing output. Switch specified with argument directs output to filepath specified by argument; no argument means output to STDOUT',
+                     nargs='?', default = 'unset' )
 args = parser.parse_args()
 
 html_file = args.html_report_filepath[0]
 
 print "parsing HTML file {0}".format( html_file )
 
-if args.graph_output_filepath is None:
-	graph_basename = html_file
+if args.figure_basename is None:
+	figure_basename = html_file
 else:
-	graph_basename = args.graph_output_filepath
+	figure_basename = args.figure_basename
 
-graph_basename, extension = os.path.splitext( graph_basename )
-graph_basename = graph_basename.replace( '.', '_' )
+figure_basename, extension = os.path.splitext( figure_basename )
+figure_basename = figure_basename.replace( '.', '_' )
 
-print "using graph basename {0}".format( graph_basename )
+print "using figure basename {0}".format( figure_basename )
 
-if args.debug is None:
+text_out = None
+if args.debug is 'unset':
+	# unset means user doesn't need debug out
+	print "User doesn't want to see debug output, sending it to /dev/null"
 	text_out = os.devnull
-else:
+elif args.debug is not None:
 	text_out = args.debug
+	print "Sending debug output to file {0}".format( text_out )
+else:
+	print "Sending debug output to STDOUT"
 
-graph = PredictedValuesGraph.NewFromHTMLFile( html_file, output_filepath=text_out )
+
+if text_out:
+	graph = PredictedValuesGraph.NewFromHTMLFile( html_file, output_filepath=text_out )
+else:
+	graph = PredictedValuesGraph.NewFromHTMLFile( html_file )
+
 graph.KernelSmoothedDensityGraph( chart_title=html_file )
-graph.SaveToFile( graph_basename + '-ks_density' )
+graph.SaveToFile( figure_basename + '-ks_density' )
 graph.RankOrderedPredictedValuesGraph( chart_title=html_file )
-graph.SaveToFile( graph_basename + '-rank-ordered' )
+graph.SaveToFile( figure_basename + '-rank-ordered' )
 
 
 
