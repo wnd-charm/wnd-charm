5d29d82035b7410ac16e48680b5526de5d0c523c
Chris Coletta <christopher.coletta@nih.gov>
2014-08-04 18:09:01 -0400

    test_WND5.py refactored and test_FeatureCalculation.py converted to unitttest

--- tests/pywndcharm_tests/test_FeatureCalculation.py
+++ tests/pywndcharm_tests/test_FeatureCalculation.py
@@ -1,80 +1,80 @@
 #!/usr/bin/env python
-# -------- preamble to get the test data --------------------
-from pychrm.FeatureSet import *
-import os
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2014 National Institutes of Health
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 import sys
-# This script is in googlecode/pychrm/trunk/tests/
-# test data in googlecode/wndchrm/tests
-# get the directory three levels up from this script, then wndchrm/tests
-my_dir = os.path.dirname(os.path.realpath(__file__))
-if (len (sys.argv) > 1):
-	test_dir = sys.argv[1]
-	if not os.path.isdir (test_dir):
-		print "The supplied path to the tests directory '{0}' is not a directory".format (test_dir)
-		test_dir = None
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
 else:
-	test_dir = os.path.join (os.path.dirname(os.path.dirname(os.path.dirname(my_dir))),'wndchrm','tests')
-	if not os.path.isdir (test_dir):
-		print "The path to the tests directory relative to this script '{0}' is not a directory".format (test_dir)
-		test_dir = None
-if not (test_dir):
-	print "The tests directory can be checked out from svn and the test re-run using the following commands:"
-	print "svn checkout http://wnd-charm.googlecode.com/svn/wndchrm/tests tests"
-	print "{0} tests".format (sys.argv[0])
-	sys.exit(0)
-from pychrm import __version__ as pychrm_version
-def roundToSigFigs(num, n):
-	from math import log10, ceil, pow, fabs
-	if(num == 0):
-		return 0;
-	d = ceil(log10(fabs(num)));
-	power = n - int(d);
-	magnitude = pow(10, power);
-	shifted = round(num*magnitude);
-	return shifted/magnitude;
+    import unittest
 
-# -------- END preamble to get the test data --------------------
+from wndcharm.FeatureSet import FeatureSet_Discrete, FisherFeatureWeights,\
+        DiscreteImageClassificationResult, Signatures
 
-test_name = "Feature calculation"
-max_diff_pass = 0.0015
-max_mean_pass = 0.00001
-# sig_file = os.path.join (test_dir,'t1_s01_c05_ij-l_precalculated.sig')
-# test_tif = os.path.join (test_dir,'t1_s01_c05_ij.tif')
-sig_file = os.path.join (test_dir,'010067_301x300-l_precalculated.sig')
-test_tif = os.path.join (test_dir,'010067_301x300.tif')
+from os.path import dirname, realpath, join
 
-test_sigs = Signatures.NewFromSigFile( sig_file, image_path = test_tif )
-#calc_sigs = Signatures.NewFromFeatureNameList ( test_tif, test_sigs.names )
-# include profiling for calculating sigs
-import cProfile
-import tempfile
-import pstats
-prof = tempfile.NamedTemporaryFile()
-cProfile.run('calc_sigs = Signatures.NewFromFeatureNameList ( test_tif, test_sigs.names )', prof.name, 'time')
-p = pstats.Stats(prof.name)
-p.sort_stats('time').print_stats(5)
-prof.close()
+pychrm_test_dir = dirname( realpath( __file__ ) ) #WNDCHARM_HOME/tests/pywndchrm_tests
+wndchrm_test_dir = join( dirname( pychrm_test_dir ), 'wndchrm_tests' )
 
-epsilon = max_diff_pass / 10.
-max_diff = 0.
-sum_diff = 0.
-num_diffs = 0.
+test_dir = wndchrm_test_dir
 
-for idx in range (len(calc_sigs.names)):
-	test_val = test_sigs.values[idx]
-	calc_val = calc_sigs.values[idx]
-	diff = abs(calc_val - test_val)
-# 	diff = abs(roundToSigFigs (calc_val,sig_figs) - roundToSigFigs (test_val,sig_figs))
-	sum_diff += diff
-	num_diffs += 1.0
-	if diff > max_diff:
-		max_diff = diff
-	if ( diff > epsilon):
-		print "computed sig '{0}' ({1}) differs from sig in file ({2}) by {3}".format (
-			calc_sigs.names [idx], calc_val, test_val, diff )
+class TestFeatureCalculation( unittest.TestCase ):
+	"""Feature Calculation"""
 
-print "{0} comparissons, maximum diff = {1}, mean = {2}".format ( int (num_diffs), max_diff, sum_diff / num_diffs )
-if (max_diff > max_diff_pass or (sum_diff / num_diffs) > max_mean_pass):
-	print "pychrm {0} {1} test: {2}".format (pychrm_version, test_name, 'FAIL')
-else:
-	print "pychrm {0} {1} test: {2}".format (pychrm_version, test_name, 'PASS')
+	epsilon = 0.00001
+
+	sig_file_path = join( test_dir,'010067_301x300-l_precalculated.sig' )
+	test_tif_path = join( test_dir,'010067_301x300.tif' )
+
+	# An alternate target for sig calculation:
+	# sig_file = os.path.join (test_dir,'t1_s01_c05_ij-l_precalculated.sig')
+	# test_tif = os.path.join (test_dir,'t1_s01_c05_ij.tif')
+
+	# --------------------------------------------------------------------------
+	@unittest.skip( "Not doing anything with this right now" )
+	def test_ProfileLargeFeatureSet( self ):
+		"""Profiling for calculating sigs"""
+
+		import cProfile
+		import tempfile
+		import pstats
+		prof = tempfile.NamedTemporaryFile()
+		cmd = 'Signatures.LargeFeatureSet( "{0}" )'.format( self.test_tif_path )
+		cProfile.run( cmd, prof.name, 'time')
+		p = pstats.Stats(prof.name)
+		p.sort_stats('time').print_stats(5)
+		prof.close()
+
+		# FIXME: Actually do some checking of the profile results
+
+	# --------------------------------------------------------------------------
+	def test_LargeFeatureSetGrayscale( self ):
+		"""Large feature set, grayscale image"""
+		reference_sample = Signatures.NewFromSigFile( self.sig_file_path,
+			image_path=self.test_tif_path )
+		target_sample = Signatures.LargeFeatureSet( self.test_tif_path )
+
+		for target_val, res_val in zip( reference_sample.values, target_sample.values ):
+			self.assertAlmostEqual( target_val, res_val, delta=self.epsilon )
+
+
+if __name__ == '__main__':
+	unittest.main()
--- tests/pywndcharm_tests/test_WND5.py
+++ tests/pywndcharm_tests/test_WND5.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Copyright (C) 2013 University of Dundee & Open Microscopy Environment.
+# Copyright (C) 2014 National Institutes of Health
 # All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -18,8 +18,6 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-#
 #
 
 import sys
@@ -28,100 +26,74 @@ if sys.version_info < (2, 7):
 else:
     import unittest
 
-from wndcharm.FeatureSet import FeatureSet_Discrete, FisherFeatureWeights,\
-        DiscreteImageClassificationResult, Signatures
-
 from os.path import dirname, realpath, join
 
 pychrm_test_dir = dirname( realpath( __file__ ) ) #WNDCHARM_HOME/tests/pywndchrm_tests
 wndchrm_test_dir = join( dirname( pychrm_test_dir ), 'wndchrm_tests' )
-
 test_dir = wndchrm_test_dir
 
+from wndcharm.FeatureSet import FeatureSet_Discrete, FisherFeatureWeights,\
+        DiscreteImageClassificationResult, Signatures
+
 class TestWND5Classification( unittest.TestCase ):
-	"""
-	WND5 Classification
-	"""
+	"""WND5 Classification"""
 
 	epsilon = 0.00001
 
 	# Define paths to original files
-
 	test_sig_path = join( test_dir,'t1_s01_c05_ij-l_precalculated.sig' )
 	test_fit_path = join( test_dir,'test-l.fit' )
 	test_feat_wght_path = join( test_dir,'test_fit-l.weights' )
 	test_tif_path = join( test_dir,'t1_s01_c05_ij.tif' )
 
-	# Here are the correct values that Python API needs to return
+	# Here are the correct values that Python API needs to return:
+	# wndchrm classify -l -f1.0 test-l.fit t1_s01_c05_ij.tif 
+	# t1_s01_c05_ij.tif	2.09e-27	0.047	0.953	*	4cell	3.906
+	# wndchrm classify -l -f0.14765 test-l.fit t1_s01_c05_ij.tif
+	# t1_s01_c05_ij.tif	4.29e-27	0.039	0.961	*	4cell	3.923
+	# wndchrm classify -l -f0.0685 test-l.fit t1_s01_c05_ij.tif
+	# t1_s01_c05_ij.tif	9.05e-27	0.032	0.968	*	4cell	3.936
 
-	# wndchrm classify -l -f1.0 test-l.fit test/2cell/t45_s06_c06_ij.tif
-	# test/2cell/t45_s06_c06_ij.tif	6.87e-28	0.614	0.386	*	2cell	2.771
-	# wndchrm classify -l -f0.14765 test-l.fit test/2cell/t45_s06_c06_ij.tif
-	# test/2cell/t45_s06_c06_ij.tif	1.34e-27	0.637	0.363	*	2cell	2.727
-	# wndchrm classify -l -f0.0685 test-l.fit test/2cell/t45_s06_c06_ij.tif
-	# test/2cell/t45_s06_c06_ij.tif	3.4e-27	0.649	0.351	*	2cell	2.701
+	correct_marg_probs = {}
+	correct_marg_probs[2919] = [0.047, 0.953]
+	correct_marg_probs[431] = [0.039, 0.961]
+	correct_marg_probs[200] = [0.032, 0.968]
 
-	correct_marg_probs_2919_feats = [0.047, 0.953]
-	correct_marg_probs_431_feats = [0.039, 0.961]
-	correct_marg_probs_200_feats = [0.032, 0.968]
+	# Load the original files once and only once for all this class's tests
+	feature_set = FeatureSet_Discrete.NewFromFitFile( test_fit_path )
+	feature_set.Normalize()
 
+	test_sample = Signatures.NewFromSigFile( test_sig_path, test_tif_path )
+	test_sample.Normalize( feature_set )
 
-	def setUp(self):
-		self.feature_set = FeatureSet_Discrete.NewFromFitFile( self.test_fit_path )
-		self.feature_set.Normalize()
+	all_weights = FisherFeatureWeights.NewFromFile( test_feat_wght_path )
 
-		self.test_sample = Signatures.NewFromSigFile( self.test_sig_path, self.test_tif_path )
-		self.test_sample.Normalize( self.feature_set )
+	# --------------------------------------------------------------------------
+	def Check( self, num_feats=None ):
+		weights = self.all_weights.Threshold( num_feats )
+		feat_set = self.feature_set.FeatureReduce( weights.names )
+		sample = self.test_sample.FeatureReduce( weights.names )
+		result = DiscreteImageClassificationResult.NewWND5( feat_set, weights, sample )
+		result_marg_probs = [ round( val, 3 ) \
+				for val in result.marginal_probabilities ]
+		for target_val, res_val in zip( self.correct_marg_probs[ num_feats ], result_marg_probs ):
+			self.assertAlmostEqual( target_val, res_val, delta=self.epsilon )
 
-		self.all_weights = FisherFeatureWeights.NewFromFile( self.test_feat_wght_path )
-
+	# --------------------------------------------------------------------------
 	def test_WND5_all_features( self ):
-		"""
-		Checks WND5 Classification with entire large feature set (2919 features)
-		"""
+		"""WND5 classification with entire large feature set (2919 features)"""
+		self.Check( 2919 )
 
-		result = DiscreteImageClassificationResult.NewWND5(
-				self.feature_set, self.all_weights, self.test_sample )
-
-		result_marg_probs = [ round( val, 3 ) \
-				for val in result.marginal_probabilities ]
-
-		for target_val, res_val in zip( self.correct_marg_probs_2919_feats, result_marg_probs ):
-			self.assertAlmostEqual( target_val, res_val, delta=self.epsilon )
-
+	# --------------------------------------------------------------------------
 	def test_WND5_15percent_threshold( self ):
-		"""
-		Checks WND5 Classification with standard 15% threshold (431) feats
-		of large feature set.
-		"""
+		"""WND5 classification with large feature set 15% threshold (431 features)"""
+		self.Check( 431 )
 
-		weights = self.all_weights.Threshold( 431 )
-		feat_set = self.feature_set.FeatureReduce( weights.names )
-		sample = self.test_sample.FeatureReduce( weights.names )
+	# --------------------------------------------------------------------------
+	def test_WND5_200_feat_threshold( self ):
+		"""WND5 classification with large feature set & 200 feature threshold"""
+		self.Check( 200 )
 
-		result = DiscreteImageClassificationResult.NewWND5( feat_set, weights, sample )
-
-		result_marg_probs = [ round( val, 3 ) \
-				for val in result.marginal_probabilities ]
-
-		for target_val, res_val in zip( self.correct_marg_probs_431_feats, result_marg_probs ):
-			self.assertAlmostEqual( target_val, res_val, delta=self.epsilon )
-
-	def test_WND5_200_feats_threshold( self ):
-		"""
-		Checks WND5 Classification with 200 feature threshold.
-		"""
-		weights = self.all_weights.Threshold( 200 )
-		feat_set = self.feature_set.FeatureReduce( weights.names )
-		sample = self.test_sample.FeatureReduce( weights.names )
-
-		result = DiscreteImageClassificationResult.NewWND5( feat_set, weights, sample )
-
-		result_marg_probs = [ round( val, 3 ) \
-				for val in result.marginal_probabilities ]
-
-		for target_val, res_val in zip( self.correct_marg_probs_200_feats, result_marg_probs ):
-			self.assertAlmostEqual( target_val, res_val, delta=self.epsilon )
 
 if __name__ == '__main__':
 	unittest.main()
